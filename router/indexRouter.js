c0ddfa421e3d184b478ad7ae8896930d7ce5b57f
Paulo2910 <paulohenvieira@outlook.com>
2023-02-12 14:37:00 -0300

    iniciando pagina de detalhes do produto

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -4,4 +4,6 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.render ('index'));
 
+router.get('/detalhes', (req, res) => res.render('./views/detalhesProdutos'));
+
 module.exports = router;
