3452b752f9468652a15bc0c79385dab2ca7565e8
Vitor Silva <silvavitoremanuel50@gmail.com>
2023-02-12 22:18:44 -0300

    Home Finalizada

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => res.render ('index'));
-
 router.get('/detalhes', (req, res) => res.render('./views/detalhesProdutos'));
 
 module.exports = router;
