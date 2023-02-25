9bca5ea8ae92b83bfe9a35b6be2ffac627f2b16f
Vitor Silva <silvavitoremanuel50@gmail.com>
2023-02-25 11:29:05 -0300

    Tela de Produtos

---
+++ controllers/produtosController.js
@@ -0,0 +1,7 @@
+const produtosController = {
+    index: (req, res ) => { 
+        res.render("detalhesProduto")
+    }
+}
+
+module.exports = produtosController;
--- router/indexRouter.js
+++ router/indexRouter.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const indexController = require('../controllers/indexController')
 const cardapioController = require('../controllers/cardapioController');
+const produtosController  = require('../controllers/produtosController')
 
 
 
 router.get('/', indexController.index);
 router.get('/cardapios', cardapioController.index)
+router.get('/produto', produtosController.index)
 //router.get('/detalhes', (req, res) => res.render('./views/detalhesProdutos'));
 
 module.exports = router;
