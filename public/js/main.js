6b654382c40a755101c676714eaa6302bb305573
Paulo2910 <paulohenvieira@outlook.com>
2023-03-13 18:51:50 -0300

    adicionando eventos de click na pagina do usuario

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', function(){
+
+    const btnDadosPessoais = document.querySelector('#btn-dados');
+    const btnEndereco = document.querySelector('#btn-endereco');
+    const btnPedidos = document.querySelector('#btn-pedidos');
+    const btnCartoes = document.querySelector('#btn-cartoes');
+
+    let dadosPessoais = document.querySelector('#dadosPessoais');
+    let endereco = document.querySelector('#endereco');
+    let pedidos = document.querySelector('#endereco');
+    let cartoes = document.querySelector('#cartoes');
+
+    function sumir (){
+        dadosPessoais.style.display = 'none';
+        endereco.style.display ='none';
+        pedidos.style.display = 'none';
+        cartoes.style.display ='none';
+    }
+
+
+    btnEndereco.addEventListener('click', function(){
+        sumir();
+        endereco.style.display ='block';
+    })
+
+    btnDadosPessoais.addEventListener('click', function(){
+        sumir();
+        dadosPessoais.style.display ='block';
+    })
+
+    btnPedidos.addEventListener('click', function(){
+        sumir();
+        pedidos.style.display = 'block'
+    })
+
+    btnCartoes.addEventListener('click', function(){
+        sumir();
+        cartoes.style.display = 'block'
+    })
+
+
+})
