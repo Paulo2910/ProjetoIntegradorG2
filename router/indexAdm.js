eca00e02502076181639410f4da813d2fe93d53d
Vitor Silva <silvavitoremanuel50@gmail.com>
2023-05-10 08:49:12 -0300

    Multer

--- router/indexAdm.js
+++ router/indexAdm.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const path =  require('path')
+const multer = require('multer')
 
 const AdminProductControllers = require('../controllers/AdminProductControllers')
 
+const multerDiskStorage = multer.diskStorage ({
+    destination: (req, file, callback) => {
+        const folder =  path.join(__dirname, '../public/img')
+        callback(null, folder);
+    },
+    filename: (req, file, callback) => {
+        const imageName = Date.now() + file.originalname;
+        callback(null, imageName)
+    }
+})
+
+const upload = multer({ storage: multerDiskStorage})
+
 router.get('/lista-de-produtos', AdminProductControllers.listProduct)
 router.get('/cadastrando-produtos', AdminProductControllers.pageProduct)
-router.post('/cadastrando-produtos', AdminProductControllers.storeProducts)
+router.post('/cadastrando-produtos', upload.single('url') , AdminProductControllers.storeProducts)
 
 //Update
 router.get('/editando-produtos/:id', AdminProductControllers.editProducts)
