ec4c40e50a311f6952fc7fdb5e4e488d62ddaf62
Vitor Silva <silvavitoremanuel50@gmail.com>
2023-05-10 23:28:24 -0300

    Update Database

--- public/js/main.js
+++ public/js/main.js
@@ -3,18 +3,18 @@ window.addEventListener('load', function(){
     const btnDadosPessoais = document.querySelector('#btn-dados');
     const btnEndereco = document.querySelector('#btn-endereco');
     const btnPedidos = document.querySelector('#btn-pedidos');
-    const btnCartoes = document.querySelector('#btn-cartoes');
+   
 
     let dadosPessoais = document.querySelector('#dadosPessoais');
     let endereco = document.querySelector('#endereco');
     let pedidos = document.querySelector('#pedidos');
-    let cartoes = document.querySelector('#cartoes');
+   
 
     function sumir (){
         dadosPessoais.style.display = 'none';
         endereco.style.display ='none';
         pedidos.style.display = 'none';
-        cartoes.style.display ='none';
+        
     }
 
 
@@ -31,11 +31,6 @@ window.addEventListener('load', function(){
     btnPedidos.addEventListener('click', function(){
         sumir();
         pedidos.style.display = 'block'
-    })
-
-    btnCartoes.addEventListener('click', function(){
-        sumir();
-        cartoes.style.display = 'block'
     })
 
 
